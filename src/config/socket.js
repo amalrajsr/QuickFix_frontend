441cc8c11c1ea0f83480a2e338e1f1b6b9c4351b
amalrajsr <sramalraj123@gmail.com>
2023-05-01 18:28:54 +0530

    modification in url

--- src/config/socket.js
+++ src/config/socket.js
@@ -1,2 +1,2 @@
- export const SOCKET_URL = "ws://localhost:7889";
-//export const SOCKET_URL = "https://quickfix-api.amalraj.tech";
+//  export const SOCKET_URL = "ws://localhost:7889";
+export const SOCKET_URL = "https://quickfix-api.amalraj.tech";
